0e3c7dac19a5c8b2330b51497e5bbec739f2ba76
Via Skillings <viaskillings@gmail.com>
2024-06-20 17:16:21 -0500

    Added comments to server folder

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,9 @@
-const path = require('path');
+const path = require('path'); // Import the 'path' module from Node.js
 
 module.exports = (app) =>
+    // Export a function that accepts 'app' as a parameter
   app.get('/', (req, res) =>
+      // Define a route handler for GET requests to '/'
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+      // Send the 'index.html' file located in the 'client/dist' directory
   );
--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require('express'); // Import the Express framework
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express(); // Create an instance of Express
+const PORT = process.env.PORT || 3000; // Define the port number from environment variable or default to 3000
 
+// Serve static files from the '../client/dist' directory
 app.use(express.static('../client/dist'));
+
+// Parse URL-encoded bodies and JSON bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Include routes defined in './routes/htmlRoutes' and pass the 'app' instance
 require('./routes/htmlRoutes')(app);
 
+// Start the server and listen on the defined port
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
