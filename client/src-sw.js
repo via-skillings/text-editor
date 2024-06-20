3a1109f84b363c6c4e721cbe81cf711f505d08d7
Via Skillings <viaskillings@gmail.com>
2024-06-20 17:11:38 -0500

    Added comments to all src

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,4 @@
+// Importing required modules from Workbox
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -5,37 +6,41 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-
+// Precache and route all assets based on the provided manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Define a CacheFirst strategy for caching pages
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: 'page-cache', // Name for the cache storage
   plugins: [
+      // Plugin to cache responses with HTTP status codes 0 and 200
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
+      // Plugin to expire cached responses after 30 days
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days in seconds
     }),
   ],
 });
 
+// Warm the cache with specific URLs using the pageCache strategy
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ['/index.html', '/'], // URLs to pre-cache
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Register a route for navigation requests using the pageCache strategy
+registerRoute(({ request }) => request.mode === 'navigate', pageCache); // Condition for navigation requests
 
-// Set up asset cache
+// Register a route for caching assets (stylesheets, scripts, workers)
 registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+    // Condition to cache requests with destination 'style', 'script', or 'worker'
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
+  new StaleWhileRevalidate({ // Name for the cache storage
     cacheName: 'asset-cache',
     plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      // Plugin to cache responses with HTTP status codes 0 and 200
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,71 +1,76 @@
+// Required webpack plugins and modules
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest} = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
+      // Set webpack mode to development
     mode: 'development',
+      // Entry points for the application bundles
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+      // Output configuration for bundled files
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js', // Output filename based on entry point names
+      path: path.resolve(__dirname, 'dist'), // Output directory path
     },
+      // Plugins used by webpack
     plugins: [
+        // Generate HTML files with injected bundles
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpackplugin'
+        template: './index.html', // HTML template file
+        title: 'Webpackplugin' // Title for the generated HTML
       }),
+        // Injects a service worker into the webpack build
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-        compileSrc: true,
-        mode: 'production',
+        swSrc: './src-sw.js', // Service worker source file
+        swDest: 'src-sw.js', // Destination file for the injected service worker
+        compileSrc: true, // Compile the source service worker file
+        mode: 'production', // Service worker mode
       }),
+        // Extracts CSS into separate files
       new MiniCssExtractPlugin(),
+        // Generates a Web App Manifest for PWA features
       new WebpackPwaManifest({
-        name: 'JATE Text Editor',
-        short_name: 'JATE',
-        description: 'A basic text editor app',
-        background_color: '#7eb4e2',
-        theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
+        name: 'JATE Text Editor', // Application name
+        short_name: 'JATE', // Short name for the application
+        description: 'A basic text editor app', // Description of the application
+        background_color: '#7eb4e2', // Background color of the application
+        theme_color: '#7eb4e2', // Theme color used for the application
+        start_url: './', // URL to start the application
+        publicPath: './', // Public path for the manifest file
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('src', 'icons'),
-          },  
+            src: path.resolve('src/images/logo.png'), // Path to the application icon
+            sizes: [96, 128, 192, 256, 384, 512], // Icon sizes
+            destination: path.join('src', 'icons'), // Destination directory for icons
+          },   
         ],
       }),
-
     ],
-
+      // Module rules for processing different file types
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          test: /\.css$/i, // CSS file matching pattern
+          use: [MiniCssExtractPlugin.loader, 'css-loader'], // Use MiniCssExtractPlugin and css-loader
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          test: /\.(png|svg|jpg|jpeg|gif)$/i, // Image file matching pattern
+          type: 'asset/resource', // Use asset/resource to handle images
         },
         {
-          test: /\.m?js$/,
-          exclude:/node_modules/,
+          test: /\.m?js$/, // JavaScript file matching pattern
+          exclude:/node_modules/, // Exclude node_modules directory
           use: {
-            loader: "babel-loader",
+            loader: "babel-loader", // Use babel-loader for JavaScript files
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'] // Babel preset options
             }
           },
         }
